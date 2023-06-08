d409f46e847a6cdc09994ee6845040a040da1494
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-06-08 10:46:03 -0300

    Realizando cadastro no banco de dados

--- src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
+++ src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
@@ -12,6 +12,7 @@ public class UsuarioSpringData {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	private String nome;
 	private String login;
 	private String senha;
 	private String email;
@@ -22,6 +23,13 @@ public class UsuarioSpringData {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	public String getLogin() {
 		return login;
--- src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
@@ -18,6 +19,14 @@ public class AppSpringDataTeste {
 	@Test
 	public void testeInsert() {
 		
-		System.out.println("Iniciou spring com sucesso");
+		UsuarioSpringData usuarioSpringData =  new UsuarioSpringData();
+		
+		usuarioSpringData.setEmail("teste4@hotmail.com");
+		usuarioSpringData.setIdade(45);
+		usuarioSpringData.setLogin("teste4");
+		usuarioSpringData.setSenha("teste4");
+		usuarioSpringData.setNome("Teste4");
+		
+		interfaceSpringDataUser.save(usuarioSpringData);
 	}
 }
